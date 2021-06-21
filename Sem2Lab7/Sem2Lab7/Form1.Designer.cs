a95d7286865004fd23c6d8824e02f29e0d73e698
DeriLone <58968194+DeriLone@users.noreply.github.com>
2021-06-21 16:48:43 +0300

    Add files via upload

---
+++ Sem2Lab7/Sem2Lab7/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Sem2Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateFolders = new System.Windows.Forms.Button();
+            this.CreateSubfolders = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SearchAndOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CreateFolders
+            // 
+            this.CreateFolders.Location = new System.Drawing.Point(65, 64);
+            this.CreateFolders.Name = "CreateFolders";
+            this.CreateFolders.Size = new System.Drawing.Size(187, 24);
+            this.CreateFolders.TabIndex = 0;
+            this.CreateFolders.Text = "Створити 100 директорій";
+            this.CreateFolders.UseVisualStyleBackColor = true;
+            this.CreateFolders.Click += new System.EventHandler(this.CreateFolders_Click);
+            // 
+            // CreateSubfolders
+            // 
+            this.CreateSubfolders.Location = new System.Drawing.Point(65, 118);
+            this.CreateSubfolders.Name = "CreateSubfolders";
+            this.CreateSubfolders.Size = new System.Drawing.Size(187, 23);
+            this.CreateSubfolders.TabIndex = 1;
+            this.CreateSubfolders.Text = "Створити вкладені директорії";
+            this.CreateSubfolders.UseVisualStyleBackColor = true;
+            this.CreateSubfolders.Click += new System.EventHandler(this.CreateSubfolders_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(122, 182);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "Видалити";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // SearchAndOpen
+            // 
+            this.SearchAndOpen.Location = new System.Drawing.Point(516, 371);
+            this.SearchAndOpen.Name = "SearchAndOpen";
+            this.SearchAndOpen.Size = new System.Drawing.Size(246, 51);
+            this.SearchAndOpen.TabIndex = 3;
+            this.SearchAndOpen.Text = "Знайти та відкрити";
+            this.SearchAndOpen.UseVisualStyleBackColor = true;
+            this.SearchAndOpen.Click += new System.EventHandler(this.SearchAndOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SearchAndOpen);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.CreateSubfolders);
+            this.Controls.Add(this.CreateFolders);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateFolders;
+        private System.Windows.Forms.Button CreateSubfolders;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button SearchAndOpen;
+    }
+}
+
---
+++ Sem2Lab7/Sem2Lab7/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sem2Lab7
+{
+    public partial class Form1 : Form
+    {
+        private const string path = @"C:\Users\Никита\source\papka";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateFolders_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            for (int i = 0; i < 100; i++)
+            {
+                dir.CreateSubdirectory($"Folder_{i}");
+            }
+            MessageBox.Show("Ok");
+        }
+
+        private void CreateSubfolders_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            int max = Int32.MaxValue;
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    dir.CreateSubdirectory($"Folder_{i}");
+                    dir = new DirectoryInfo(dir.FullName + $"\\Folder_{i}");
+                }
+                catch (PathTooLongException)
+                {
+                    max = i + 1;
+                    break;
+                }
+            }
+            MessageBox.Show("Ok");
+            MessageBox.Show($"Max: {max + 1}");
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            foreach (var folder in dir.GetDirectories())
+            {
+                folder.Delete(true);
+            }
+            MessageBox.Show("Deleted");
+        }
+
+        private void SearchAndOpen_Click(object sender, EventArgs e)
+        {
+            string fileContent = string.Empty;
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    Stream fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+    }
+}
